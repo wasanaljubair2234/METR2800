587df510d8d0e923eaa051151e6c76f29ef37d1d
wasanaljubair2234 <w.aaljubair@student.uq.edu.au>
2026-05-07 16:35:20 +1000

    Create sensing.ino

---
+++ sensing.ino
@@ -0,0 +1,41 @@
+// ultrasonic sensor and search logic
+
+// helper function for distance
+float getDistanceCm() {
+  
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(trig_pin, LOW);
+  
+  float timing = pulseIn(echo_pin, HIGH); // measures return pulse time
+  
+  return (timing * 0.034) / 2;  // converts pulse time to cm
+}
+
+
+void search() {   
+
+  rotation_stepper.step(-50);  // rotate sensor to intial position
+  delay(300);
+  
+  for (int i = 0; i < 50; i++) {
+    // rotate incrementally while scanning
+    rotation_stepper.step(1);  // move motor 1 step , 50 steps is 90 degrees, then scan distance
+    delay(10);
+
+    detected_distance = getDistanceCm();
+
+    if (detected_distance < distance) { // collects and stores values for closest angle and distance 
+      distance = detected_distance;
+      angle = current_angle; 
+    }
+
+  }
+
+  rotations_stepper.step(-1*(50 - angle)) // rotate back to be in line with tube
+}
+
