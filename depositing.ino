872327d7bfd8dcdf108eb9bb17e754925b1d9fc8
wasanaljubair2234 <w.aaljubair@student.uq.edu.au>
2026-05-10 12:09:43 +1000

    Update depositing.ino

--- depositing.ino
+++ depositing.ino
@@ -1,16 +1,15 @@
 // rock deposition 
 // uses the detected hopper distance returned by getDistanceCm() from the sensing/search function 
 // need to convert angle_1 and angle_2 into step counts
-// arm commands can only be confirmed once stepper drivers are confirmed
 
 void deposit(float distance) { 
 
   float y = 360; // vertical distance between arm base and hopper
 
   // need to be measured
-  float length_1 = ; 
-  float length_2 = ; 
-  float scoop_width = ; 
+  float length_1 = #; 
+  float length_2 = #; 
+  float scoop_width = #; 
 
   // reach calculations
   float length_3 = sqrt(length_2*length_2 + Scoop_width*Scoop_width );
@@ -21,7 +20,7 @@ void deposit(float distance) {
   float angle_1 = atan2(y, distance) - atan(length_3 *sin(angle_2_offset), length_1 + length_3 * cos(angle_2_offset));
   float angle_2 = angle_2_offset - angle_offset;
   
-  // acuate servos 
+  // acuate steppers 
   // actuate angle 1, then 2  include some factor phi_1, phi_2 etc to calibrate realworld tests
 
   int limb1Steps = #;
